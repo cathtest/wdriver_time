5fbf16f6eb1ea7b9ed3721486b403e40cd5c1556
Katsiaryna Hrynchuk <rina957200@gmail.com>
2018-07-23 01:49:51 +0300

    changes

---
+++ src/main/java/services/CalendarService.java
@@ -0,0 +1,61 @@
+package services;
+
+import blocks.Calendar;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+
+public class CalendarService {
+
+    private Calendar calendar;
+
+    public CalendarService() {
+        this.calendar = new Calendar();
+    }
+
+    public String getTextFromCalendarButton(){
+       return calendar.getCalendarButton().getText();
+    }
+
+    public void calendarButtonClick(){
+        calendar.getCalendarButton().click();
+    }
+
+    public void clickNotCurrentTimeRange(){
+        calendar.getTimeRangeNotCurrent().click();
+    }
+
+    public void openLastYearOnTheList(){
+        calendar.getLastYear().click();
+    }
+
+    public void FollowingYearClick(){
+        calendar.getTheFollowingYear().click();
+    }
+
+    public void firstMonthInTheYearClick(){
+        calendar.getTheFirstMonth().click();
+    }
+
+    public void firstDayInTheMonthClick(){
+        calendar.getTheFirstDay().click();
+    }
+
+    public void changeRange(){
+        calendar.getAnotherView().click();
+    }
+
+    public String getTextFromRange(){
+        return calendar.getAnotherView().getText();
+    }
+    public void rightArrowClick(){
+        calendar.getRightArrow().click();
+    }
+
+    public void leftArrowClick(){
+        calendar.getLeftArrow().click();
+    }
+
+    public List<WebElement> getListOfYears(){
+        return calendar.getYearsList();
+    }
+}
